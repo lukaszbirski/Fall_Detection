8d82e17f282f4c1081460b28cecbddae0a58d384
Lukasz Birski <lukasz.birski@gmail.com>
2022-07-13 22:20:29 +0200

    Created unit tests for usecases and contact mapper

---
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/model/util/ContactMapperTest.kt
@@ -0,0 +1,52 @@
+package com.example.android.architecture.blueprints.todoapp.model.util
+
+import com.example.android.architecture.blueprints.todoapp.data.model.ContactEntity
+import com.example.android.architecture.blueprints.todoapp.data.source.AppDatabaseFake
+import com.example.android.architecture.blueprints.todoapp.model.Contact
+import org.junit.Before
+import org.junit.Test
+
+class ContactMapperTest {
+
+    private val contactsEntity = AppDatabaseFake().contacts
+
+    // system in test
+    private lateinit var mapper: ContactMapper
+
+    @Before
+    fun setup() {
+        mapper = ContactMapper()
+    }
+
+    @Test
+    fun `map list of contactEntity to list of contacts`() {
+        val contacts = mapper.mapToDomainModelList(contactsEntity)
+
+        // confirm that list is not empty
+        assert(contacts.isNotEmpty())
+
+        // confirm they are actually Contact objects
+        assert(contacts.get(index = 0) is Contact)
+    }
+
+    @Test
+    fun `map contactEntity to contact`() {
+        val contacts = mapper.mapToDomainModel(contactsEntity[0])
+
+        // confirm that mapped object is Contact type
+        assert(contacts is Contact)
+    }
+
+    @Test
+    fun `map contact to contact entity`() {
+        val contacts = mapper.mapToDomainModel(contactsEntity[0])
+
+        // confirm that mapped object is Contact type
+        assert(contacts is Contact)
+
+        val contactEntity = mapper.mapFromDomainModel(contacts)
+
+        // confirm that mapped object is ContactEntity type
+        assert(contactEntity is ContactEntity)
+    }
+}
---
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/usecase/RemoveContactUseCaseTest.kt
@@ -0,0 +1,54 @@
+package com.example.android.architecture.blueprints.todoapp.usecase
+
+import com.example.android.architecture.blueprints.todoapp.data.source.AppDatabaseFake
+import com.example.android.architecture.blueprints.todoapp.data.source.ContactDaoFake
+import com.example.android.architecture.blueprints.todoapp.model.util.ContactMapper
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class RemoveContactUseCaseTest {
+
+    private val appDatabase = AppDatabaseFake()
+
+    // system in test
+    private lateinit var removeContactUseCase: RemoveContactUseCase
+
+    // dependencies
+    private lateinit var contactDao: ContactDaoFake
+    private val mapper = ContactMapper()
+
+    private lateinit var getAllContactsUseCase: GetAllContactsUseCase
+
+    // want to remove first element from database
+    private val contactToRemove = mapper.mapToDomainModel(appDatabase.contacts[0])
+
+    @Before
+    fun setup() {
+        contactDao = ContactDaoFake(appDatabase)
+
+        removeContactUseCase = RemoveContactUseCase(
+            mapper = mapper,
+            contactDao = contactDao
+        )
+
+        getAllContactsUseCase = GetAllContactsUseCase(
+            mapper = mapper,
+            contactDao = contactDao
+        )
+    }
+
+    @Test
+    fun `remove contact from database`(): Unit = runBlocking {
+        removeContactUseCase.execute(contact = contactToRemove)
+
+        val contacts = getAllContactsUseCase.execute().toList()
+
+        // confirm that we removed element from database
+        assert(contactToRemove !in contacts)
+    }
+
+    @After
+    fun tearDown() { }
+}
