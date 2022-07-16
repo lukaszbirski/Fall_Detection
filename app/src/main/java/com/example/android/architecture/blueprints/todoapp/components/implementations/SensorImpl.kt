6aa9c8ae3b8594051633a4c43ad473668e3edac8
Lukasz Birski <lukasz.birski@gmail.com>
2022-07-16 17:29:47 +0200

    Created FallDetector

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/components/implementations/SensorImpl.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/components/implementations/SensorImpl.kt
@@ -9,23 +9,26 @@ import android.widget.Toast
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import com.example.android.architecture.blueprints.todoapp.R
+import com.example.android.architecture.blueprints.todoapp.components.interfaces.FallDetector
 import com.example.android.architecture.blueprints.todoapp.model.Acceleration
 import com.example.android.architecture.blueprints.todoapp.other.Constants
 import timber.log.Timber
 import javax.inject.Inject
 
-class SensorImpl @Inject constructor() : com.example.android.architecture.blueprints.todoapp.components.interfaces.Sensor, SensorEventListener {
+class SensorImpl @Inject constructor(
+    private val fallDetector: FallDetector
+) : com.example.android.architecture.blueprints.todoapp.components.interfaces.Sensor, SensorEventListener {
 
     private var manager: SensorManager? = null
 
     private val acceleration: MutableState<Acceleration?> = mutableStateOf(null)
-    private var rawAcceleration = Acceleration()
 
     override fun onSensorChanged(event: SensorEvent) {
         when (event.sensor.type) {
             Sensor.TYPE_ACCELEROMETER -> {
-                rawAcceleration = createAcceleration(event = event)
+                val rawAcceleration = createAcceleration(event = event)
                 acceleration.value = rawAcceleration
+                fallDetector.detectFall(rawAcceleration)
                 Timber.d("Raw acceleration is equal to: $rawAcceleration")
             }
         }
--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/di/AccelerometerModule.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/di/AccelerometerModule.kt
@@ -1,12 +1,16 @@
 package com.example.android.architecture.blueprints.todoapp.di
 
+import android.content.Context
+import com.example.android.architecture.blueprints.todoapp.components.implementations.FallDetectorImpl
 import com.example.android.architecture.blueprints.todoapp.components.implementations.FilterImpl
 import com.example.android.architecture.blueprints.todoapp.components.implementations.SensorImpl
+import com.example.android.architecture.blueprints.todoapp.components.interfaces.FallDetector
 import com.example.android.architecture.blueprints.todoapp.components.interfaces.Filter
 import com.example.android.architecture.blueprints.todoapp.components.interfaces.Sensor
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -16,7 +20,16 @@ object AccelerometerModule {
 
     @Singleton
     @Provides
-    fun provideSensor(): Sensor = SensorImpl()
+    fun provideSensor(
+        fallDetector: FallDetector
+    ): Sensor = SensorImpl(fallDetector)
+
+    @Singleton
+    @Provides
+    fun provideFallDetector(
+        @ApplicationContext context: Context,
+        filter: Filter
+    ): FallDetector = FallDetectorImpl(context, filter)
 
     @Singleton
     @Provides
