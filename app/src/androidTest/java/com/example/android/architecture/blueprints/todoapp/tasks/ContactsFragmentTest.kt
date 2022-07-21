2c7b10db0eb35576c4e560aa65aad0e692fb21f4
Lukasz Birski <lukasz.birski@gmail.com>
2022-07-22 00:38:29 +0200

    added test responsible for adding new contact to list

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/tasks/ContactsFragmentTest.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/tasks/ContactsFragmentTest.kt
@@ -1,9 +1,15 @@
 package com.example.android.architecture.blueprints.todoapp.tasks
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.RootMatchers.isDialog
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.isEnabled
+import androidx.test.espresso.matcher.ViewMatchers.isRoot
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import com.example.android.architecture.blueprints.todoapp.R
@@ -12,6 +18,7 @@ import com.example.android.architecture.blueprints.todoapp.presentation.fragment
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.hamcrest.Matchers.not
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -39,5 +46,43 @@ class ContactsFragmentTest {
         onView(withId(R.id.addItemTitleTextView)).check(matches(isDisplayed()))
         onView(withId(R.id.contactsRecycler)).check(matches(isDisplayed()))
         onView(withId(R.id.addContactButton)).check(matches(isDisplayed()))
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun addContact_newElementsIsDisplayedInList() {
+        launchFragmentInHiltContainer<ContactsFragment> {}
+
+        // check if button is displayed
+        onView(withId(R.id.addContactButton)).check(matches(isDisplayed()))
+        // presses button
+        onView(withId(R.id.addContactButton)).perform(click())
+        // checks if dialog is displayed
+        onView(isRoot()).inRoot(isDialog()).check(matches(isDisplayed()))
+        // checks if cancel button is enabled and add button disabled
+        onView(withText(R.string.contact_dialog_add_text)).check(matches(not(isEnabled())))
+        onView(withText(R.string.contact_dialog_cancel_text)).check(matches(isEnabled()))
+        // types name
+        onView(withId(R.id.nameEditText)).perform(typeText("Jan"))
+        // checks if cancel button is enabled and add button disabled
+        onView(withText(R.string.contact_dialog_add_text)).check(matches(not(isEnabled())))
+        onView(withText(R.string.contact_dialog_cancel_text)).check(matches(isEnabled()))
+        // types surname
+        onView(withId(R.id.surnameEditText)).perform(typeText("Nowak"))
+        // checks if cancel button is enabled and add button disabled
+        onView(withText(R.string.contact_dialog_add_text)).check(matches(not(isEnabled())))
+        onView(withText(R.string.contact_dialog_cancel_text)).check(matches(isEnabled()))
+        // types prefix
+        onView(withId(R.id.prefixEditText)).perform(typeText("42"))
+        // checks if cancel button is enabled and add button disabled
+        onView(withText(R.string.contact_dialog_add_text)).check(matches(not(isEnabled())))
+        onView(withText(R.string.contact_dialog_cancel_text)).check(matches(isEnabled()))
+        // types phone number
+        onView(withId(R.id.phoneEditText)).perform(typeText("123456789"))
+        // checks if both buttons are enabled
+        onView(withText(R.string.contact_dialog_add_text)).check(matches(isEnabled()))
+        onView(withText(R.string.contact_dialog_cancel_text)).check(matches(isEnabled()))
+        // clicks add button
+        onView(withText(R.string.contact_dialog_add_text)).perform(click())
     }
 }
